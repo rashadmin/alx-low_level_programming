2e7d11e04fff3549822efbde3b7674663a929721
rashadmin <abdul.a.rasheed2021@gmail.com>
2023-07-14 17:22:42 +0100

    added all task bug

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
-a C program that prints exactly "Programming is like building a multilingual puzzle, 
-followed by a new line
+*a C program that prints exactly "Programming is like building a multilingual puzzle, 
+*followed by a new line
 */
 int main(void)
 {
