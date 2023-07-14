87aad98c416adf9781aac08e8cbec3119b83c2c8
rashadmin <abdul.a.rasheed2021@gmail.com>
2023-07-14 20:34:04 +0100

    fixed bug

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ double doubleType;
 char charType;
 
     /* Sizeof operator is used to evaluate the size of a variable */
-printf("Size of char: %ld byte(s)\n", sizeof(char));
-printf("Size of int: %ld byte(s)\n", sizeof(int));
-printf("Size of long int: %ld byte(s)\n", sizeof(long int));
-printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
-printf("Size of float: %ld byte(s)\n", sizeof(float));
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %d byte(s)\n", sizeof(long int));
+printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of float: %d byte(s)\n", sizeof(float));
 
 return (0);
 }
