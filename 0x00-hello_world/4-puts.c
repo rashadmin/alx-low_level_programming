4eb41332222b2a9270f48a77ce882b229a2ce850
rashadmin <abdul.a.rasheed2021@gmail.com>
2023-07-14 17:06:35 +0100

    added all task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	char Mystr[100] = "\"Programming is like building a multilingual puzzle";
+	puts(Mystr);
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	char Mystr[100] = "with proper grammar, but the outcome is a piece of art\n";
+	printf("%s",Mystr);
+	return 0;
+}
