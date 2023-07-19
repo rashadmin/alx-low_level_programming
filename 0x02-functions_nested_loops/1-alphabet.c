5e5a16aded163ab9e480d70f2d37a86338dd5959
rashadmin <abdul.a.rasheed2021@gmail.com>
2023-07-19 07:12:02 +0100

    fixed bug]

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,6 @@
  *
  * Return: Always 0 (success)
  */
-
 void print_alphabet(void)
 {	
 	int character;
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,9 +7,9 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+void print_alphabet(void);
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-void print_alphabet(void);
 
