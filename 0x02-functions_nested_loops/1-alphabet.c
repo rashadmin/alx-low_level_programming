f23645d9485ee15908f306371c26bf4311b4950f
rashadmin <abdul.a.rasheed2021@gmail.com>
2023-07-20 02:43:52 +0100

    fixed bug

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main - prints string
+ * print_alphabet - prints string
  *
  * Return: Always 0 (success)
  */
 void print_alphabet(void)
-{	
+{
 	int character;
-	for (character=97;character <= 122;character++)
-	{ 
+
+	for (character = 97; character <= 122; character++)
+{
 	_putchar(character);
-	}
+}
 	_putchar('\n');
 }
